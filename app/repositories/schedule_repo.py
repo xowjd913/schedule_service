9856541a004b308608cae517782e634332f34792
TJKim <xowjd913@gmail.com>
2026-01-02 23:44:56 +0900

    🔨[Update]: schedule repository 추가

---
+++ app/repositories/schedule_repo.py
@@ -0,0 +1,29 @@
+from sqlalchemy.orm import Session
+from typing import List, Optional
+
+from app.domain.schedule import Schedule
+
+class ScheduleRepository:
+    def create(self, db: Session, schedule: Schedule) -> Schedule:
+        db.add(schedule)
+        db.commit()
+        db.refresh(schedule)
+
+        return schedule
+    
+    def get_by_id(self, db: Session, schedule_id: int) -> Optional[Schedule]:
+        return {
+            db.query(Schedule)
+            .filter(Schedule.id == schedule_id)
+            .first()
+        }
+    
+    def get_all(self, db: Session) -> List[Schedule]:
+        return db.query(Schedule).all()
+    
+    def save(self, db: Session, schedule: Schedule) -> Schedule:
+        db.add(schedule)
+        db.commit()
+        db.refresh(schedule)
+
+        return schedule
